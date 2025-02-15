e42d9ae8bf9bf58599f9552b35f5a1841ba530e6
gjohnn <juansgb316@outlook.com>
2025-02-14 21:18:31 -0300

    Only base code

---
+++ HELP.md
@@ -0,0 +1,5 @@
+#Spring Security 6
+
+Hi, this is my new Spring Security 6 microservice. I want to improve the older version that is a public repository of mine.
+
+##### I WILL CHANGE THIS MESSAGE WHEN IT'S DONE
---
+++ src/main/java/com/jgiga/SpringSecurity/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package com.jgiga.SpringSecurity.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class HelloController {
+    
+
+    @GetMapping("/")
+    public String getHelloString () {
+        return "Hola g__john";
+    }
+    
+    
+    
+}
