00657483e9de9849e685656852a02b379e216753
gjohnn <juansgb316@outlook.com>
2025-02-15 21:02:42 +0000

    JWT added and completed

--- src/main/java/com/jgiga/SpringSecurity/config/Auth/SecurityConfig.java
+++ src/main/java/com/jgiga/SpringSecurity/config/Auth/SecurityConfig.java
@@ -1,24 +1,23 @@
-package com.jgiga.SpringSecurity.config;
+package com.jgiga.SpringSecurity.config.Auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
 
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.jgiga.SpringSecurity.config.Auth.JWT.JwtFilter;
 import com.jgiga.SpringSecurity.services.CustomUserDetailsService;
 
 @Configuration
@@ -26,19 +25,21 @@ import com.jgiga.SpringSecurity.services.CustomUserDetailsService;
 public class SecurityConfig {
 
     @Autowired
-    private UserDetailsService userDetailsService;
+    private JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http.csrf(customizer -> customizer.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/register").permitAll()
-                        .requestMatchers("/students").authenticated() // 🔒
-                        .anyRequest().authenticated() // Proteger el resto
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated() // 🔒
                 )
                 .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
+
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
 
     }
 
@@ -48,6 +49,11 @@ public class SecurityConfig {
         provider.setUserDetailsService(userDetailsService);
         provider.setPasswordEncoder(new BCryptPasswordEncoder(12)); // Asegurar que usa BCrypt
         return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
 }
