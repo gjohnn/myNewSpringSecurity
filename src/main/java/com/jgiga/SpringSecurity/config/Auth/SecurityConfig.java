13c3265767aba06afd768bd7eeb5aef8ac6d96a5
gjohnn <juansgb316@outlook.com>
2025-02-19 22:51:32 +0000

    new cors config (NOT TESTED)

--- src/main/java/com/jgiga/SpringSecurity/config/Auth/SecurityConfig.java
+++ src/main/java/com/jgiga/SpringSecurity/config/Auth/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.jgiga.SpringSecurity.config.Auth;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.jgiga.SpringSecurity.config.Auth.JWT.JwtFilter;
 import com.jgiga.SpringSecurity.services.CustomUserDetailsService;
@@ -26,12 +31,16 @@ public class SecurityConfig {
     private JwtFilter jwtFilter;
 
     // This method configures the security filter chain for HTTP requests.
-    // It disables CSRF protection, allows unrestricted access to /auth/** endpoints,
-    // requires authentication for all other requests, uses HTTP Basic authentication,
-    // sets the session management policy to stateless, and adds the JWT filter before the UsernamePasswordAuthenticationFilter.
+    // It disables CSRF protection, allows unrestricted access to /auth/**
+    // endpoints,
+    // requires authentication for all other requests, uses HTTP Basic
+    // authentication,
+    // sets the session management policy to stateless, and adds the JWT filter
+    // before the UsernamePasswordAuthenticationFilter.
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(customizer -> customizer.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated() // 🔒
@@ -53,9 +62,25 @@ public class SecurityConfig {
     }
 
     // This method configures the authentication manager.
-    // It retrieves the authentication manager from the authentication configuration.
+    // It retrieves the authentication manager from the authentication
+    // configuration.
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    // CORS
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Frontend en React
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
